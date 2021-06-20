39e29cb358250cbdfc2e38bb8b8a9f3f63785eed
keithkaykay <kaykaykeith@yahoo.com>
2021-06-21 01:29:56 +0300

    make different classes for the different portfolio images

--- js/script.js
+++ js/script.js
@@ -30,3 +30,67 @@ $(document).ready(function(){
     $("#product-image").slideDown('1500');
   });
 });
+
+$(document).ready(function(){
+  $("#bella-1").mouseover(function(){
+    $("#cathew-1").show();
+  }).mouseout(function(){
+    $("#cathew-1").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#bella-2").mouseover(function(){
+    $("#cathew-2").show();
+  }).mouseout(function(){
+    $("#cathew-2").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#bella-3").mouseover(function(){
+    $("#cathew-3").show();
+  }).mouseout(function(){
+    $("#cathew-3").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#bella-4").mouseover(function(){
+    $("#cathew-4").show();
+  }).mouseout(function(){
+    $("#cathew-4").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#bella-5").mouseover(function(){
+    $("#cathew-5").show();
+  }).mouseout(function(){
+    $("#cathew-5").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#bella-6").mouseover(function(){
+    $("#cathew-6").show();
+  }).mouseout(function(){
+    $("#cathew-6").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#bella-7").mouseover(function(){
+    $("#cathew-7").show();
+  }).mouseout(function(){
+    $("#cathew-7").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#bella-8").mouseover(function(){
+    $("#cathew-8").show();
+  }).mouseout(function(){
+    $("#cathew-8").hide();
+  });
+});
